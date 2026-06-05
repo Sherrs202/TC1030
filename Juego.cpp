bad943ad036fdcfc5131be8a293d009e1cf4f3e2
Sherrs202 <A01714794@tec.mx>
2026-06-04 21:59:06 -0600

    Refactor Juego class methods and structure

--- Juego.cpp
+++ Juego.cpp
@@ -9,32 +9,213 @@ Juego::Juego() {
 
     dia = 1;
 
-    jugador.setNombre("Gerardo");
+    bebidas.push_back(
+        Bebida("Coca Cola",5,15,5)
+    );
 
-    bebidas.push_back(Bebida("Coca Cola", 5, 15, 5));
-    bebidas.push_back(Bebida("Agua", 3, 10, 5));
-    bebidas.push_back(Bebida("Monster", 10, 25, 3));
+    bebidas.push_back(
+        Bebida("Agua",3,10,5)
+    );
+
+    bebidas.push_back(
+        Bebida("Monster",10,25,3)
+    );
 
 }
 
+void Juego::mostrarInventario() {
+
+    cout << "\nInventario\n";
+
+    for(int i=0;i<bebidas.size();i++) {
+
+        cout
+        << i+1
+        << ". "
+        << bebidas[i].getNombre()
+        << " Cantidad: "
+        << bebidas[i].getCantidad()
+        << endl;
+
+    }
+
+}
+
+void Juego::comprarBebidas() {
+
+    int opcion;
+    int cantidad;
+
+    mostrarInventario();
+
+    cout << "\nSelecciona bebida: ";
+    cin >> opcion;
+
+    cout << "Cantidad: ";
+    cin >> cantidad;
+
+    opcion--;
+
+    int costo =
+        bebidas[opcion].getPrecioCompra()
+        * cantidad;
+
+    if(jugador.getDinero() >= costo) {
+
+        jugador.comprar(costo);
+
+        bebidas[opcion].comprar(cantidad);
+
+        cout << "Compra realizada\n";
+
+    }
+    else {
+
+        cout << "Dinero insuficiente\n";
+
+    }
+
+}
+
+void Juego::eventoPersona() {
+
+    Cliente cliente;
+    Bandido bandido;
+
+    Persona* persona;
+
+    int tipo = rand() % 2;
+
+    if(tipo == 0) {
+
+        persona = &cliente;
+
+    }
+    else {
+
+        persona = &bandido;
+
+    }
+
+    cout << "\n===== EVENTO =====\n" << endl;
+
+    persona->accion();
+
+    // CLIENTE
+    if(tipo == 0) {
+
+        int opcion;
+
+        cout << "\n¿Quieres atender al cliente?" << endl;
+        cout << "1. Si" << endl;
+        cout << "2. No" << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+
+        if(opcion == 1) {
+
+            int bebida = rand() % bebidas.size();
+
+            cout << "\nEl cliente pidio: "
+                 << bebidas[bebida].getNombre()
+                 << endl;
+
+            if(bebidas[bebida].getCantidad() > 0) {
+
+                bebidas[bebida].vender();
+
+                jugador.vender(
+                    bebidas[bebida].getPrecioVenta()
+                );
+
+                cout << "\nVenta realizada." << endl;
+
+                cout << "Ganaste $"
+                     << bebidas[bebida].getPrecioVenta()
+                     << endl;
+
+            }
+            else {
+
+                cout << "\nYa no queda esa bebida." << endl;
+
+            }
+
+        }
+        else {
+
+            cout << "\nEl cliente se fue sin comprar." << endl;
+
+        }
+
+    }
+
+    // BANDIDO
+    else {
+
+        int opcion;
+
+        cout << "\n¿Quieres defenderte?" << endl;
+        cout << "1. Si" << endl;
+        cout << "2. No" << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+
+        if(opcion == 1) {
+
+            if(jugador.getArma()) {
+
+                cout << "\nSacaste tu arma." << endl;
+                cout << "El bandido huyo." << endl;
+
+            }
+            else {
+
+                cout << "\nIntentaste defenderte..." << endl;
+                cout << "Pero no tienes arma." << endl;
+
+                cout << "\nEl bandido te robo $50." << endl;
+
+                jugador.comprar(50);
+
+            }
+
+        }
+        else {
+
+            cout << "\nDecidiste no defenderte." << endl;
+
+            cout << "El bandido te robo $50." << endl;
+
+            jugador.comprar(50);
+
+        }
+
+    }
+
+}
 void Juego::menu() {
 
     int opcion;
 
-    bool jugando = true;
+    do {
 
-    while(jugando) {
+        cout << "\n======== DIA "
+             << dia
+             << " ========\n";
 
-        cout << "\n=========== DIA " << dia << " ===========" << endl;
+        cout
+        << "Dinero: $"
+        << jugador.getDinero()
+        << endl;
 
-        cout << "\nDinero: $" << jugador.getDinero() << endl;
-
-        cout << "\n1. Ver inventario";
+        cout << "\n1. Inventario";
         cout << "\n2. Comprar bebidas";
-        cout << "\n3. Esperar clientes";
-        cout << "\n4. Comprar arma ($100)";
+        cout << "\n3. Evento";
+        cout << "\n4. Comprar arma";
         cout << "\n5. Dormir";
         cout << "\n6. Salir";
+
         cout << "\n\nOpcion: ";
 
         cin >> opcion;
@@ -53,7 +234,7 @@ void Juego::menu() {
 
         case 3:
 
-            esperarClientes();
+            eventoPersona();
             break;
 
         case 4:
@@ -64,12 +245,12 @@ void Juego::menu() {
 
                 jugador.conseguirArma();
 
-                cout << "\nCompraste un arma" << endl;
+                cout << "Compraste un arma\n";
 
             }
             else {
 
-                cout << "\nNo tienes suficiente dinero" << endl;
+                cout << "No tienes dinero suficiente\n";
 
             }
 
@@ -77,216 +258,14 @@ void Juego::menu() {
 
         case 5:
 
-            cout << "\nTerminando dia..." << endl;
-
             dia++;
 
-            eventoRandom();
+            cout << "Terminando dia...\n";
 
             break;
 
-        case 6: {
-
-            int salir;
-
-            cout << "\nQuieres salir?" << endl;
-            cout << "1. Si" << endl;
-            cout << "2. No" << endl;
-
-            cin >> salir;
-
-            if(salir == 1) {
-
-                jugando = false;
-
-                cout << "\nFIN DEL JUEGO" << endl;
-
-                cout << "Llegaste al dia "
-                     << dia
-                     << endl;
-
-                cout << "Dinero final: $"
-                     << jugador.getDinero()
-                     << endl;
-
-            }
-
-            break;
         }
 
-        default:
-
-            cout << "\nOpcion invalida" << endl;
-
-        }
-
-    }
-
-}
-
-void Juego::mostrarInventario() {
-
-    cout << "\n======= INVENTARIO =======\n";
-
-    for(int i = 0; i < bebidas.size(); i++) {
-
-        cout << i + 1 << ". "
-             << bebidas[i].getNombre()
-             << " | Cantidad: "
-             << bebidas[i].getCantidad()
-             << endl;
-
-    }
-
-}
-
-void Juego::comprarBebidas() {
-
-    int opcion;
-    int cantidad;
-
-    mostrarInventario();
-
-    cout << "\nQue bebida quieres comprar?: ";
-    cin >> opcion;
-
-    cout << "Cantidad: ";
-    cin >> cantidad;
-
-    opcion--;
-
-    int costo =
-        bebidas[opcion].getPrecioCompra()
-        * cantidad;
-
-    if(jugador.getDinero() >= costo) {
-
-        jugador.comprar(costo);
-
-        bebidas[opcion].comprar(cantidad);
-
-        cout << "\nCompra realizada" << endl;
-
-    }
-    else {
-
-        cout << "\nNo tienes suficiente dinero" << endl;
-
-    }
-
-}
-
-void Juego::esperarClientes() {
-
-    int opcion;
-
-    cout << "\nUn cliente se acerco..." << endl;
-
-    cout << "\nQuieres vender?" << endl;
-    cout << "1. Si" << endl;
-    cout << "2. No" << endl;
-
-    cin >> opcion;
-
-    switch(opcion) {
-
-    case 1: {
-
-        int random = rand() % bebidas.size();
-
-        Cliente cliente;
-
-        cliente.setNombre("Cliente");
-
-        Persona* persona = &cliente;
-
-        persona->accion();
-
-        cout << "\nPidio: "
-             << bebidas[random].getNombre()
-             << endl;
-
-        if(bebidas[random].getCantidad() > 0) {
-
-            bebidas[random].vender();
-
-            jugador.vender(
-                bebidas[random].getPrecioVenta()
-            );
-
-        }
-        else {
-
-            cout << "\nNo queda esa bebida" << endl;
-
-        }
-
-        break;
-    }
-
-    case 2:
-
-        cout << "\nEl cliente se fue" << endl;
-        break;
-
-    default:
-
-        cout << "\nOpcion invalida" << endl;
-
-    }
-
-}
-
-void Juego::eventoRandom() {
-
-    int evento = rand() % 4;
-
-    switch(evento) {
-
-    case 0:
-
-        cout << "\nNo paso nada hoy" << endl;
-        break;
-
-    case 1:
-
-        cout << "\nUn cliente rico compro mucho" << endl;
-
-        jugador.agregarDinero(50);
-
-        break;
-
-    case 2: {
-
-        cout << "\nUn bandido aparecio" << endl;
-
-        Bandido bandido;
-
-        Persona* persona = &bandido;
-
-        persona->accion();
-
-        if(jugador.getArma()) {
-
-            cout << "\nTe defendiste del robo" << endl;
-
-        }
-        else {
-
-            cout << "\nTe robaron $50" << endl;
-
-            jugador.comprar(50);
-
-        }
-
-        break;
-    }
-
-    case 3:
-
-        cout << "\nLlovio mucho hoy" << endl;
-        break;
-
-    }
+    } while(opcion != 6);
 
 }
