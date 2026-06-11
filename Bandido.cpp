32f78af6afe05d36c45199d24b80c14a80db5963
Sherrs202 <A01714794@tec.mx>
2026-06-10 20:56:28 -0600

    Update Bandido.cpp

--- Bandido.cpp
+++ Bandido.cpp
@@ -3,12 +3,16 @@
 
 using namespace std;
 
+// Constructor por default de la calse Bandido
 Bandido::Bandido() {
 
     nombre = "Bandido";
 
 }
 
+/**
+ * Muestra la acción realizada por un bandido
+*/
 void Bandido::accion() {
 
     cout << "El bandido intenta robarte." << endl;
