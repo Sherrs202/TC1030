8553e60eb1ca5d566441fee410ae557f6980ba8a
Sherrs202 <A01714794@tec.mx>
2026-06-10 20:55:11 -0600

    Implement default constructor for Arma class
    Removed old comments and header, added constructor implementation.

--- Arma.cpp
+++ Arma.cpp
@@ -1,29 +1,9 @@
-/*
- *
- * Proyecto Taberna
- * Gerardo Silva
- * TC1030 - Programación Orientada a Objetos
- * Fecha: 2026
- * Versión: 1.0
- * Esta clase define un objeto de tipo Arma que puede ser
- * adquirido por el jugador para aumentar sus posibilidades
- * de defenderse de los bandidos.
- *
- */
+#include "Arma.h"
 
-#ifndef ARMA_H
-#define ARMA_H
+// Constructor por default de la calse Arma
+Arma::Arma() {
 
-#include "Item.h"
+    nombre = "Pistola";
+    precio = 100;
 
-// Clase que representa un arma y hereda de Item
-class Arma : public Item {
-
-public:
-
-    // Constructor por default de la calse Arma
-    Arma();
-
-};
-
-#endif
+}
