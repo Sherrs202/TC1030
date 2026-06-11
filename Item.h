f537912f417b4e9720f6e60f8cdf2dc703a1c95d
Sherrs202 <A01714794@tec.mx>
2026-06-10 21:00:01 -0600

    Refactor Item.h with documentation and comments
    Added header comments and improved class documentation.

--- Item.h
+++ Item.h
@@ -1,3 +1,15 @@
+/*
+ *
+ * Proyecto Taberna
+ * Gerardo Silva
+ * TC1030 - Programación Orientada a Objetos
+ * Fecha: 2026
+ * Versión: 1.0
+ * Esta clase define un objeto de tipo Item que sirve como
+ * clase base para los objetos disponibles en el juego.
+ *
+ */
+
 #ifndef ITEM_H
 #define ITEM_H
 
@@ -5,15 +17,20 @@
 
 using namespace std;
 
+// Clase base para los obejtps disponibles en el juego
 class Item {
 
 protected:
 
+    //Nombre del item
     string nombre;
+
+    // Precio del objeto
     int precio;
 
 public:
 
+    // Constructor por default
     Item();
 
 };
