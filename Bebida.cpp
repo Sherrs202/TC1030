5156dd2dfdd9af6cfa968e204adbea427c24a60b
Sherrs202 <A01714794@tec.mx>
2026-06-10 20:57:38 -0600

    Add comments to Bebida class methods
    Added documentation comments for class methods.

--- Bebida.cpp
+++ Bebida.cpp
@@ -1,8 +1,17 @@
 #include "Bebida.h"
-
+// Constructor por default de la clase Bebida
 Bebida::Bebida() {
 
 }
+
+/**
+ * Inicializa una bebida con sus datos principales
+ * 
+ * @param n nombre de la bebida
+ * @param pc precio de compra de la bebida
+ * @param pv precio de venta de la bebida   
+ * @param c cantidad inicial disponible 
+*/
 
 Bebida::Bebida(string n, int pc, int pv, int c) {
 
@@ -13,36 +22,46 @@ Bebida::Bebida(string n, int pc, int pv, int c) {
 
 }
 
+// Regresa la cantidad disponible
 int Bebida::getCantidad() {
 
     return cantidad;
 
 }
 
+// Regresa el precio de venta
 int Bebida::getPrecioVenta() {
 
     return precioVenta;
 
 }
 
+// regresa el precio de compra
 int Bebida::getPrecioCompra() {
 
     return precioCompra;
 
 }
 
+// Regresa el nombre de la bebida
 string Bebida::getNombre() {
 
     return nombre;
 
 }
 
+/*
+ * Aumenta la cantidad disponible en el inventario
+ *
+ * @param cant cantidad de bebidas a agregar
+*/
 void Bebida::comprar(int cant) {
 
     cantidad += cant;
 
 }
 
+// Disminuye la cantidad disponible despues de una venta
 void Bebida::vender() {
 
     if(cantidad > 0) {
