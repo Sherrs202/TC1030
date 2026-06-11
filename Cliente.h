936925be64cdcdd0ac67cb02750b4a774f2f7402
Sherrs202 <A01714794@tec.mx>
2026-06-10 20:58:58 -0600

    Refactor Cliente.h with comments and documentation
    Added documentation and comments to the Cliente class.

--- Cliente.h
+++ Cliente.h
@@ -1,14 +1,30 @@
+/*
+ *
+ * Proyecto Taberna
+ * Gerardo Silva
+ * TC1030 - Programación Orientada a Objetos
+ * Fecha: 2026
+ * Versión: 1.0
+ * Esta clase define un objeto de tipo Cliente que representa
+ * a las personas que visitan la taberna para comprar bebidas.
+ *
+ */
+
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
 #include "Persona.h"
 
+// Clase que representa a un cliente y hereda de Persona
 class Cliente : public Persona {
 
 public:
 
     Cliente();
 
+    /**
+     * Implementa la acción realizada por un cliente.
+     */
     void accion();
 
 };
