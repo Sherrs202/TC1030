9bce71d8e4aa057ebce4b8ef500b7b9697025bc7
Sherrs202 <A01714794@tec.mx>
2026-06-10 20:58:31 -0600

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -3,12 +3,16 @@
 
 using namespace std;
 
+// Constructor por default de la clase Cliente
 Cliente::Cliente() {
 
     nombre = "Cliente";
 
 }
 
+/**
+ * Muestra la acción realizada por un cliente.
+ */
 void Cliente::accion() {
 
     cout << "El cliente quiere comprar una bebida." << endl;
