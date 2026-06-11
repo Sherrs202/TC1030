dcee1cafabcc8df030e00d5d6eb1459591d09346
Sherrs202 <A01714794@tec.mx>
2026-06-10 21:03:11 -0600

    Refactor Persona.h with comments and class details
    Added documentation comments and improved class description.

--- Persona.h
+++ Persona.h
@@ -1,3 +1,15 @@
+/*
+ *
+ * Proyecto Taberna
+ * Gerardo Silva
+ * TC1030 - Programación Orientada a Objetos
+ * Fecha: 2026
+ * Versión: 1.0
+ * Esta clase define un objeto de tipo Persona que sirve como
+ * clase base abstracta para los personajes del juego.
+ *
+ */
+
 #ifndef PERSONA_H
 #define PERSONA_H
 
@@ -5,21 +17,35 @@
 
 using namespace std;
 
+// Clase abstracta que representa a una persona dentro del juego
 class Persona {
 
 protected:
 
+    // Datos básicos de la persona
     string nombre;
     int vida;
 
 public:
 
+    // Constructor por default
     Persona();
 
+    /**
+     * Asigna un nombre a la persona.
+     *
+     * @param n nuevo nombre de la persona.
+     */
     void setNombre(string n);
 
+    // Regresa el nombre de la persona
     string getNombre();
 
+    /**
+     * Método abstracto que define una acción.
+     *
+     * Será implementado por las clases derivadas.
+     */
     virtual void accion() = 0;
 
 };
