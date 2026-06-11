3029fa92717e90deaebfdf119f607c7e9f308fad
Sherrs202 <A01714794@tec.mx>
2026-06-10 20:53:41 -0600

    Add documentation and include guards to Arma class
    Added header comments and include guards to Arma.cpp.

--- Arma.cpp
+++ Arma.cpp
@@ -1,8 +1,29 @@
-#include "Arma.h"
+/*
+ *
+ * Proyecto Taberna
+ * Gerardo Silva
+ * TC1030 - Programación Orientada a Objetos
+ * Fecha: 2026
+ * Versión: 1.0
+ * Esta clase define un objeto de tipo Arma que puede ser
+ * adquirido por el jugador para aumentar sus posibilidades
+ * de defenderse de los bandidos.
+ *
+ */
 
-Arma::Arma() {
+#ifndef ARMA_H
+#define ARMA_H
 
-    nombre = "Pistola";
-    precio = 100;
+#include "Item.h"
 
-}
+// Clase que representa un arma y hereda de Item
+class Arma : public Item {
+
+public:
+
+    // Constructor por default de la calse Arma
+    Arma();
+
+};
+
+#endif
